cf2e202bb7ae9bdc5d58d52b34ffa2913b8ac1f8
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2022-01-27 00:13:42 +0100

    Code cleanup and added features: - setting brightness in runtime, - setting rtc calibration in runtime

--- fw/ledclock.c
+++ fw/ledclock.c
@@ -11,9 +11,9 @@
  * - time setting via two buttons (one for minutes, one for hours),
  * - long press button to set time faster,
  * - blinking after power loss to indicate that the time is incorrect,
- * - RTC calibration with 1 ppm precision (+- 1023 ppms, hardcoded),
+ * - RTC calibration with 1 ppm precision (+- 999 ppms),
  * - slow, gradual enabling/disabling changed screen segments (PWM),
- * - brightness setting (hardcoded),
+ * - brightness setting (0-7),
  * - watchdog.
  *
  * Copyright 2022 Aleksander Kaminski
@@ -29,11 +29,11 @@
 #define BUTTON_COOLDOWN  200  /* In about 1 ms */
 #define BUTTON_LONGPRESS 2000 /* In about 1 ms */
 #define LONGPRESS_HZ     4    /* How fast is autopress working */
-#define BRIGHTNESS       250  /* Duty of LCD being on (x/256) */
+#define BRIGHTNESS       140  /* Base brightness (x/256) */
 #define RTC_CALIB        0    /* +-ppm */
 #define RTC_HZ           1024
 #define RAMP_MIN         16   /* Minimal PWM (x/256) */
-#define RAMP_MAX         (BRIGHTNESS - 16)
+#define RAMP_MAX         (BRIGHTNESS + (g_brightness * 16) - 16)
 #define RAMP_INC         4    /* Increased on every screen refresh (122 Hz) */
 
 
@@ -59,12 +59,60 @@ unsigned int g_button_presscnt[2];
 enum {
 	button_not_active,
 	button_active,
-	button_longpress } g_button_state[2] = {
-		button_not_active, button_not_active
-	};
+	button_longpress,
+	button_lockup
+} g_button_state[2] = { button_not_active, button_not_active };
+
+enum {
+	mode_normal = 0,
+	mode_calib,
+	mode_brightness,
+	mode_end
+} g_mode = mode_normal;
+byte g_mode_timeout;
+int g_rtc_calib = RTC_CALIB;
+byte g_brightness = 4;
 
 
-static void _hours_inc(void)
+static void set_brightness(void)
+{
+	OCR0B = BRIGHTNESS + (g_brightness * 16);
+}
+
+
+static void brightness_inc(void)
+{
+	if (g_brightness < 8)
+		++g_brightness;
+
+	set_brightness();
+}
+
+
+static void brightness_dec(void)
+{
+	if (g_brightness > 0)
+		--g_brightness;
+
+	set_brightness();
+}
+
+
+static void calib_inc(void)
+{
+	if (g_rtc_calib < 1000)
+		++g_rtc_calib;
+}
+
+
+static void calib_dec(void)
+{
+	if (g_rtc_calib > -1000)
+		--g_rtc_calib;
+}
+
+
+static void hours_inc(void)
 {
 	byte hours_term = 10;
 
@@ -81,7 +129,7 @@ static void _hours_inc(void)
 }
 
 
-static void _minutes_inc(void)
+static void minutes_inc(void)
 {
 	if (++g_minutes_l < 10)
 		return;
@@ -91,7 +139,7 @@ static void _minutes_inc(void)
 		return;
 	g_minutes_h = 0;
 
-	_hours_inc();
+	hours_inc();
 }
 
 
@@ -101,7 +149,7 @@ static byte button_check(byte which)
 }
 
 
-static byte _button_handle(byte which)
+static byte button_handle(byte which)
 {
 	byte trigger = 0;
 
@@ -115,7 +163,7 @@ static byte _button_handle(byte which)
 				trigger = 1;
 			}
 		}
-		else {
+		else if (g_button_state[which] != button_lockup) {
 			g_button_state[which] = button_longpress;
 		}
 	}
@@ -141,14 +189,17 @@ static byte decode7seg(byte dig)
 		~0x07 & 0x7f, /* 7 */
 		~0x7f & 0x7f, /* 8 */
 		~0x6f & 0x7f, /* 9 */
-		~0x00 & 0x7f  /* void */
+		~0x00 & 0x7f, /* void */
+		~0x7c & 0x7f, /* b */
+		~0x39 & 0x7f, /* c */
+		~0x79 & 0x7f  /* e */
 	};
 
 	return lut[dig];
 }
 
 
-static void _update_digit(byte which, byte newval)
+static void update_digit(byte which, byte newval)
 {
 	g_led_on[which] |= g_led_rampup[which];
 	g_led_on[which] &= ~g_led_rampdown[which];
@@ -160,7 +211,7 @@ static void _update_digit(byte which, byte newval)
 }
 
 
-static void _set_ramp(byte val)
+static void set_ramp(byte val)
 {
 	if (val < RAMP_MIN)
 		val = RAMP_MIN;
@@ -172,26 +223,42 @@ static void _set_ramp(byte val)
 }
 
 
-static void set_brightness(byte val)
+static void refresh_screen(int blanking)
 {
-	OCR0B = val;
-}
+	switch (g_mode) {
+		case mode_calib:
+			if (g_rtc_calib >= 0)
+				update_digit(0, decode7seg(0xc));
+			else
+				update_digit(0, decode7seg(0xe));
 
+			update_digit(1, decode7seg((g_rtc_calib / 100) % 10));
+			update_digit(2, decode7seg((g_rtc_calib / 10) % 10));
+			update_digit(3, decode7seg(g_rtc_calib % 10));
+			break;
 
-static void _refresh_screen(int blanking)
-{
-	if (blanking) {
-		for (byte i = 0; i < 4; ++i)
-			_update_digit(i, 10);
+		case mode_brightness:
+			update_digit(0, decode7seg(0xb));
+			update_digit(1, decode7seg(10)); /* void */
+			update_digit(2, decode7seg(10)); /* void */
+			update_digit(3, decode7seg(g_brightness));
+			break;
+
+		default:
+			if (blanking) {
+				for (byte i = 0; i < 4; ++i)
+					update_digit(i, 10); /* void */
+			}
+			else {
+				update_digit(0, decode7seg(g_hours_h));
+				update_digit(1, decode7seg(g_hours_l));
+				update_digit(2, decode7seg(g_minutes_h));
+				update_digit(3, decode7seg(g_minutes_l));
+			}
+			break;
 	}
-	else {
-		_update_digit(0, decode7seg(g_hours_h));
-		_update_digit(1, decode7seg(g_hours_l));
-		_update_digit(2, decode7seg(g_minutes_h));
-		_update_digit(3, decode7seg(g_minutes_l));
-	}
 
-	_set_ramp(RAMP_MIN);
+	set_ramp(RAMP_MIN);
 }
 
 
@@ -199,6 +266,36 @@ static void set_dots(int state)
 {
 	PORTB &= ~(!state << 7);
 	PORTB |= !state << 7;
+}
+
+
+static void button_action(byte which)
+{
+	switch (g_mode) {
+		case mode_calib:
+			if (!which)
+				calib_inc();
+			else
+				calib_dec();
+			break;
+
+		case mode_brightness:
+			if (!which)
+				brightness_inc();
+			else
+				brightness_dec();
+			break;
+
+		default:
+			if (!which)
+				minutes_inc();
+			else
+				hours_inc();
+			g_seconds = 0;
+			break;
+	}
+
+	g_mode_timeout = 0;
 }
 
 
@@ -211,7 +308,8 @@ ISR(INT0_vect)
 	if (++g_subseconds >= RTC_HZ) {
 		g_subseconds -= RTC_HZ;
 		if (++g_seconds >= 60) {
-			_minutes_inc();
+			g_seconds = 0;
+			minutes_inc();
 			update = 1;
 		}
 
@@ -234,20 +332,34 @@ ISR(INT0_vect)
 		}
 	}
 
-	if ((btrigger = _button_handle(0))) {
-		_minutes_inc();
+	if ((btrigger = button_handle(0))) {
+		button_action(0);
 	}
-	else if ((btrigger = _button_handle(1))) {
-		_hours_inc();
+	else if ((btrigger = button_handle(1))) {
+		button_action(1);
 	}
-	else if (!(g_subseconds % (RTC_HZ / LONGPRESS_HZ))) {
+	else if (g_button_state[0] == button_longpress &&
+			g_button_state[1] == button_longpress) {
+		if (++g_mode == mode_end)
+			g_mode = mode_normal;
+
+		g_button_state[0] = g_button_state[1] = button_lockup;
+		update = 1;
+	}
+	else if (g_mode == mode_normal &&
+			!(g_subseconds % (RTC_HZ / LONGPRESS_HZ))) {
 		btrigger = 1;
 		if (g_button_state[0] == button_longpress)
-			_minutes_inc();
+			button_action(0);
 		else if (g_button_state[1] == button_longpress)
-			_hours_inc();
+			button_action(1);
 		else
 			btrigger = 0;
+	}
+
+	if (g_mode != mode_normal && ++g_mode_timeout > 5) {
+		g_mode = mode_normal;
+		update = 1;
 	}
 
 	if (btrigger) {
@@ -255,10 +367,8 @@ ISR(INT0_vect)
 		g_time_set = 1;
 	}
 
-	if (update) {
-		g_seconds = 0;
-		_refresh_screen(blanking);
-	}
+	if (update)
+		refresh_screen(blanking);
 }
 
 
@@ -290,7 +400,7 @@ ISR(TIMER0_COMPB_vect)
 	g_curr_digit = (g_curr_digit + 1) % 4;
 
 	if (!g_curr_digit)
-		_set_ramp(g_rampcnt + RAMP_INC);
+		set_ramp(g_rampcnt + RAMP_INC);
 }
 
 
@@ -304,7 +414,7 @@ int main(void)
 	DDRB = 0xff;
 	PORTD |= 0xf << 3;
 	DDRD |= 0xf << 3;
-	_refresh_screen(0);
+	refresh_screen(0);
 
 	/* Buttons - inputs, pull-up enable */
 	PORTD |= (1 << 1) | (1 << 0);
@@ -320,8 +430,8 @@ int main(void)
 	/* Timer0 - screen management */
 	/* Update OCRx at MAX */
 	TCCR0A = (1 << WGM01) | (1 << WGM00);
-	_set_ramp(RAMP_MIN);
-	set_brightness(BRIGHTNESS);
+	set_ramp(RAMP_MIN);
+	set_brightness();
 	TIMSK |= (1 << OCIE0B) | (1 << TOIE0) | (1 << OCIE0A);
 	/* Enable counter (1/64 prescaler) */
 	TCCR0B = (1 << CS01) | (1 << CS00);
